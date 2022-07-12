fe469392fc638d20f32a8ef8bd24e3ed302523aa
lty <lty826224862@163.com>
2022-07-12 11:22:02 +0800

    first Date:2022 0712 11:21

---
+++ SpringBoot-01-Web/src/main/java/com/lty/SpringBoot01WebApplication.java
@@ -0,0 +1,13 @@
+package com.lty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot01WebApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-01-Web/src/test/java/com/lty/SpringBoot01WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot01WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
